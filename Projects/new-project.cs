c73b7d8f98481dc7b98a51f374460d3e2a95dcb0
Stanislav Chorbadzhiev <stchorbadzhiev@gmail.com>
2018-03-08 23:08:40 +0200

    Modified file
    Modified file

---
+++ Projects/new-project.cs
@@ -0,0 +1,15 @@
+namespace GithubDesktopTest
+{
+	class Program
+	{
+		static void Main()
+		{
+			System.Console.WriteLine("Hello world!");
+			
+			for(int i = 0; i<10; i++)
+			{
+				System.Console.WriteLine(i);
+			}
+		}		
+	}
+}
